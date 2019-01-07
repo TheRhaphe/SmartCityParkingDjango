a8e8caf0c711d355cdc83ccf83a3a207e0194e5e
TheRhaphe <bmahatchi@gmail.com>
2019-01-07 20:06:18 +0200

    Initial upload

---
+++ mysite/sensors/views.py
@@ -0,0 +1,12 @@
+# Create your views here.
+from django.shortcuts import render
+#from .models import Question
+
+
+def index(request):
+    """
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+    """
+    return render(request, 'sensors/home.html')
